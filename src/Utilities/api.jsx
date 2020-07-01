e53f3b84497a2df1b99b2908e1ad417a9a281714
Shane Bodimer <shanefbodimer@gmail.com>
2020-06-30 22:24:23 -0400

    Removed location

--- src/Utilities/api.jsx
+++ src/Utilities/api.jsx
@@ -1,33 +1,6 @@
 import React from "react";
 import axios from "axios";
 
-// If no location stored
-if (!localStorage.getItem("state")) {
-  // Get position, call function
-  navigator.geolocation.getCurrentPosition(showPosition);
-
-  // Get state from position
-  function showPosition(position) {
-    var url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${
-      position.coords.latitude + "," + position.coords.longitude
-    }&key=AIzaSyBWeMU1MHTGjl4QKTW7cUPZHOvXd5_zkfk`;
-    var method = "GET";
-    var shouldBeAsync = true;
-    var request = new XMLHttpRequest();
-    request.open(method, url, shouldBeAsync);
-    request.send();
-
-    // Execute on load and store
-    request.onload = function () {
-      var data = JSON.parse(request.responseText);
-      localStorage.setItem(
-        "state",
-        data.results[0].address_components[5].short_name
-      );
-    };
-  }
-}
-
 // Login
 export const search = (query) =>
   axios
@@ -54,7 +27,6 @@ export const searchHot = () =>
       {
         headers: {
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "http://localhost:3000",
         },
       }
     )
